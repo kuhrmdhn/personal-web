65f58baa65033a64ec5359517b8ea0712a5d98e2
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-08-13 08:03:37 +0700

    edit project card ui

--- src/app/ui/components/Project/ProjectCard.tsx
+++ src/app/ui/components/Project/ProjectCard.tsx
@@ -18,10 +18,10 @@ export default function ProjectCard({ project }: Props) {
     }
     return (
         <section className='w-80 lg:w-96 h-[360px] lg:h-[420px] border-mainBlue border rounded-lg shadow-sm shadow-mainBlue p-3 flex flex-col justify-between'>
-            <div className='h-1/4 w-full text-start'>
+            <div className='h-1/3 w-full text-start flex flex-col justify-between'>
                 <h1 className='text-xl font-bold'>{name}</h1>
-                <p className='text-sm'>{description}</p>
-                <ul className="overflow-x-auto flex gap-3 text-xs mt-1">
+                <p className='text-xs sm:text-sm'>{description}</p>
+                <ul className="overflow-x-auto flex gap-3 text-xs mt-1 scrollbar-hide">
                     {
                         stacks.map((stack, index) => (
                             <li key={index}>
@@ -36,7 +36,7 @@ export default function ProjectCard({ project }: Props) {
             <div className='w-full h-1/2 flex justify-center'>
                 <Image src={image} alt={name} height={120} width={360} className='h-full aspect-video rounded-xl' />
             </div>
-            <div className='flex justify-end items-end gap-3'>
+            <div className='h-fit flex justify-end items-end gap-3'>
                 <TailwindConnectButton text="Github" onClick={() => visitPage(github_url)} />
                 <TailwindConnectButton text="Demo" onClick={() => visitPage(demo_url)} />
             </div>
