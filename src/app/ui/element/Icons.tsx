222c038b4053df6ac34c1194b5956b18b4d46e08
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-10-05 22:03:19 +0700

    setting ascending project

--- src/app/ui/element/Icons.tsx
+++ src/app/ui/element/Icons.tsx
@@ -13,7 +13,7 @@ export default function Icons({ iconSource, link, className }: Props) {
     const { name, image } = iconSource
     return (
         <a href={link} target="_blank" rel="noopener noreferrer" title={name}>
-            <Image src={image} alt={name} height={20} width={20} className={`cursor-pointer relative z-40 ${className}`} />
+            <Image src={image || ""} alt={name} height={20} width={20} className={`cursor-pointer relative z-40 ${className}`} />
         </a>
     )
 }
--- src/database/projectListRouter.ts
+++ src/database/projectListRouter.ts
@@ -4,10 +4,8 @@ import { useProjectStore } from "@/lib/zustand/useProjectStore"
 export const projectRouter = {
     async getProjectList(limit?: number) {
         try {
-            if (!limit) {
-                limit = 2
-            }
-            const { data: limitedProject, error } = await supabase.from("project_list").select().range(0, limit)
+            limit = limit || 2
+            const { data: limitedProject, error } = await supabase.from("project_list").select().order("id", { ascending: true }).range(0, limit)
             if (error) throw new Error(error.message)
             const sortedProjects = limitedProject.sort((a, b) => a.id - b.id)
             useProjectStore.setState({ projectList: sortedProjects })
