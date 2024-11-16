b29e00efb594dca86b3c58175b98194f5365c4fd
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-11-16 09:58:11 +0700

    add academic section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import Header from './ui/components/Header.vue';
-import Home from './ui/components/Home.vue';
-import About from './ui/components/About.vue';
-import Navigation from './ui/elements/Navigation.vue';
-import Contact from './ui/components/Contact.vue';
-import Skill from './ui/components/Skill.vue';
-import Project from './ui/components/Project.vue';
-import Footer from './ui/components/Footer.vue';
+import About from "./ui/components/About.vue";
+import Contact from "./ui/components/Contact.vue";
+import Academic from "./ui/components/Academic.vue";
+import Footer from "./ui/components/Footer.vue";
+import Header from "./ui/components/Header.vue";
+import Home from "./ui/components/Home.vue";
+import Project from "./ui/components/Project.vue";
+import Skill from "./ui/components/Skill.vue";
+import Navigation from "./ui/elements/Navigation.vue";
 </script>
 
 <template>
@@ -15,9 +16,10 @@ import Footer from './ui/components/Footer.vue';
     <Header />
     <Home />
     <About />
-    <Skill/>
-    <Project/>
-    <Contact/>
-    <Footer/>
+    <Skill />
+    <Academic/>
+    <Project />
+    <Contact />
+    <Footer />
   </div>
 </template>
