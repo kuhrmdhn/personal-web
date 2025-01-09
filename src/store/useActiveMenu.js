b20824e5f523e00f44d37d67ecc9e50bc231f043
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-01-09 20:01:52 +0700

    [fix] Visible Element loop

--- src/store/useActiveMenu.js
+++ src/store/useActiveMenu.js
@@ -4,7 +4,7 @@ import { ref } from "vue";
 export const useActiveMenu = defineStore("activeMenu", () => {
   const isActive = ref(false);
   function toggleMenu() {
-      isActive.value = !isActive.value;
+        isActive.value = !isActive.value;
   }
 
   return { isActive, toggleMenu };
